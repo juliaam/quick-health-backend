5eb659150c47c96e1d5ed2dabd773a87bdd69f2b
juliaam <juliademoraess@gmail.com>
2025-09-15 19:45:21 -0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://quick-health-ui.vercel.app/login',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT ?? 3000);
